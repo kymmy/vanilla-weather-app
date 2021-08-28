d830f5f52ea0290f37c21576546c4e8e0c2bcba8
kymmy <55324981+kymmy@users.noreply.github.com>
2021-08-28 13:44:21 +0200

    Add search engine

--- src/app.js
+++ src/app.js
@@ -44,9 +44,19 @@ function displayTempertaure(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "078db4e136e212a8147d77a8cd2054b4";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+function search(city) {
+  let apiKey = "078db4e136e212a8147d77a8cd2054b4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTempertaure);
+}
 
-axios.get(apiUrl).then(displayTempertaure);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
